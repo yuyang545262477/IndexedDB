9c15f18d48675c87bf68a0904391961f7a7deb11
David_Yu <yuyang545262477@gmail.com>
2015-09-21 06:53:28 +0800

    0921代码初步走完，添坑路漫漫。并且代码优化很有空间

--- dist/js/js/main.js
+++ dist/js/js/main.js
@@ -141,7 +141,163 @@
             }
         };
         openDB();
+        //创建动态列表 5-11
+        var createEmptyItem = function (query, taskList) {
+            var emptyItem = document.createElement('li');
+            if (query.length > 0) {
+                emptyItem.innerHTML = '<div class = "item_title">' + 'No tasks match your query <strong>' + query + '</strong>' + '</div>';
+            } else {
+                emptyItem.innerHTML = '<div class = "item_title">' + 'No tasks to display.<a href="#add">Add one</a>?' + '</div>';
+            }
+            taskList.appendChild(emptyItem);
+        };
+        var showTask = function (task, list) {
+            var newItem = document.createElement('li'),
+                checked = (task.complete == 1) ? 'checked ="checked"' : '';
 
+            newItem.innerHTML =
+            //div.item_complete
+            '<div class="item_complete">' +
+            '<input type="checkbox" name="item_complete"' +
+            'id = "chk_' + task.id +
+            ' " ' + checked + '>' + '</div>' +
+            //div.item_delete
+            '<div class = "item_delete">' + '<a href+"#" id = "del_' + task.id + '">Delete</a>' + '</div>' +
+            //div.item_title
+            '<div class = "item_title">' + task.desc + '</div>' +
+            //div.item_due
+            '<div class = "item_due">' + task.due + '<div>';
+            list.appendChild(newItem);
+
+            var markAsComplete = function (e) {
+                e.preventDefault();
+                var updatedTask = {
+                    id: task.id,
+                    desc: task.desc,
+                    descUpper: task.desc.toUpperCase(),
+                    due: task.due,
+                    complete: e.traget.checked
+                };
+                updateTask(updatedTask);//之后讲到updateTask方法。
+            };
+
+            var remove = function (e) {
+                e.preventDefault();
+                if (confirm('Deleting task. Are you sure?', 'Delete')) {
+                    deleteTask(task.id);
+                }
+            };
+
+            document.getElementById('chk_' + task.id).onchange = markAsComplete;
+            document.getElementById('del_' + task.id).onclick = remove;
+        };
+
+        //实现数据库的搜索
+        var loadTasks = function (q) {
+            var taskList = document.getElementById('task_list'),
+                query = q || '';
+            taskList.innerHTML = '';
+
+            if (indexedDB) {
+                //建立数据事务
+                var tx = db.transaction(['tasks'], 'readonly'),
+                    objectStore = tx.objectStore('tasks'),
+                    cursor,
+                    i = 0;
+
+                if (query.length > 0) {
+                    var index = objectStore.index('desc'),
+                        upperQ = query.toUpperCase(),
+                        keyRange = IDBKeyRange.bound(upperQ, upperQ + 'z');
+                    cursor = index.openCursor(keyRange);
+                } else {
+                    cursor = objectStore.openCursor();
+                }
+
+                cursor.onsuccess = function (e) {
+                    var result = e.target.result;
+                    if (result === null) return;
+                    i++;
+                    showTask(result.value, taskList);
+                    result['continue']();
+                };
+
+                tx.concomplete = function (e) {
+                    if (i === 0) { createEmptyItem(query, taskList); }
+                };
+            } else {
+                alert("your brower is not support IndexedDB");
+            }
+        };
+        //新建searchTasks
+        var searchTasks = function (e) {
+            e.preventDefault();
+            var query = document.forms.search.query.value;
+            if (query.length > 0) {
+                loadTasks(query);
+            } else {
+                loadTasks();
+            }
+        };
+        //添加提交后的searchTasks事件绑定.
+        document.forms.search.addEventListener('submit', searchTasks, false);
+
+        //新建 insertTasks
+        var insertTasks = function (e) {
+            e.preventDefault();
+            var desc = document.forms.add.desc.value,
+                dueDate = document.forms.add.due_date.value;
+            if (desc.length > 0 && dueDate.length > 0) {
+                var task = {
+                    id: new Date().getTime(),
+                    desc: desc,
+                    descUpper: desc.toUpperCase,
+                    due: dueDate,
+                    complete: false
+                };
+
+                if (indexedDB) {
+                    var tx = db.transaction(['task'], 'readwrite');
+                    var objectStore = tx.objectStore('tasks');
+                    var request = objectStore.add(task);
+                    tx.concomplete = updateView;
+                } else {
+                    alert('insertTasks not support');
+                }
+
+            } else {
+                alert('please fill out all fields', 'Add task error');
+            }
+        };
+        function updateView() {
+            loadTasks();
+            alert('Task added successfully', 'Task added');
+            document.forms.add.desc.value = '';
+            document.forms.add.due_date.value = '';
+            location.hash = '#list';
+        }
+        document.forms.add.addEventListener('submit', insertTasks, false);
+        //新建updateTasks
+        var updatedTask = function (task) {
+            //从这里开始忽略indexedDB是否支持的判断
+            var tx = db.transaction(['tasks'], 'readwrite'),
+                objectStore = tx.objectStore('tasks'),
+                request = objectStore.put(task);
+            //怀疑此处少代码.
+
+        };
+        //新建deleteTask
+        var deleteTask = function (id) {
+            var tx = db.transaction(['tasks'], 'readwrite'),
+                objectStore = tx.objectStore('tasks'),
+                request = objectStore['delete'](id);
+            tx.concomplete = loadTasks;
+        };
+        //新建dropDatabase
+        var dropDatabase = function () {
+            var delDBRequest = indexedDB.deleteDatabase('tasks');
+            delDBRequest.onsuccess = window.location.reload();
+        };
 
     };
     window.addEventListener('load', function () {
